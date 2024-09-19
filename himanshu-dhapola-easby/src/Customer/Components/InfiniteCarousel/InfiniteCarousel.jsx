c0404582e5f13138d3012467e2dba43cbecb3348
Himanshu Dhapola <himanshu.dhapola.work@gmail.com>
2024-09-19 21:39:39 +0530

    Update InfiniteCarousel.jsx

--- himanshu-dhapola-easby/src/Customer/Components/InfiniteCarousel/InfiniteCarousel.jsx
+++ himanshu-dhapola-easby/src/Customer/Components/InfiniteCarousel/InfiniteCarousel.jsx
@@ -3,31 +3,31 @@ export default function InfiniteCarousel() {
     <div className="w-full inline-flex flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-200px),transparent_100%)] my-8 bg-smoke">
       <ul className="flex items-center justify-center md:justify-start sm:gap-6 gap-4 [&_li]:mx-4 [&_img]:max-w-none animate-infinite-scroll">
         <li>
-          <img src="./samsung.png" alt="Samsung" className="w-12 md:w-24" />
+          <img src="./samsung.png" alt="Samsung" loading="lazy"  className="w-12 md:w-24" />
         </li>
         <li>
-          <img src="./intel.png" alt="Intel" className="w-12 md:w-24" />
+          <img src="./intel.png" alt="Intel" loading="lazy"  className="w-12 md:w-24" />
         </li>
         <li>
-          <img src="./apple.png" alt="Apple" className="w-12 md:w-24" />
+          <img src="./apple.png" alt="Apple" loading="lazy"  className="w-12 md:w-24" />
         </li>
         <li>
-          <img src="./adidas.png" alt="Adidas" className="w-12 md:w-24" />
+          <img src="./adidas.png" alt="Adidas" loading="lazy"  className="w-12 md:w-24" />
         </li>
         <li>
-          <img src="./sony.png" alt="Sony" className="w-12 md:w-24" />
+          <img src="./sony.png" alt="Sony" loading="lazy"  className="w-12 md:w-24" />
         </li>
         <li>
-          <img src="./asus.png" alt="Asus" className="w-12 md:w-24" />
+          <img src="./asus.png" alt="Asus" loading="lazy"  className="w-12 md:w-24" />
         </li>
         <li>
-          <img src="./redmi.png" alt="Redmi" className="w-12 md:w-24" />
+          <img src="./redmi.png" alt="Redmi" loading="lazy"  className="w-12 md:w-24" />
         </li>
         <li>
-          <img src="./bose.png" alt="Bose" className="w-12 md:w-24" />
+          <img src="./bose.png" alt="Bose" loading="lazy"  className="w-12 md:w-24" />
         </li>
         <li>
-          <img src="./hp.png" alt="Hp" className="w-12 md:w-24" />
+          <img src="./hp.png" alt="Hp" loading="lazy"  className="w-12 md:w-24" />
         </li>
       </ul>
       <ul
@@ -35,31 +35,31 @@ export default function InfiniteCarousel() {
         aria-hidden="true"
       >
         <li>
-          <img src="./samsung.png" alt="Samsung" className="w-12 md:w-24" />
+          <img src="./samsung.png" alt="Samsung" loading="lazy"  className="w-12 md:w-24" />
         </li>
         <li>
-          <img src="./intel.png" alt="Intel" className="w-12 md:w-24" />
+          <img src="./intel.png" alt="Intel" loading="lazy"  className="w-12 md:w-24" />
         </li>
         <li>
-          <img src="./apple.png" alt="Apple" className="w-12 md:w-24" />
+          <img src="./apple.png" alt="Apple" loading="lazy"  className="w-12 md:w-24" />
         </li>
         <li>
-          <img src="./adidas.png" alt="Adidas" className="w-12 md:w-24" />
+          <img src="./adidas.png" alt="Adidas" loading="lazy"  className="w-12 md:w-24" />
         </li>
         <li>
-          <img src="./sony.png" alt="Sony" className="w-12 md:w-24" />
+          <img src="./sony.png" alt="Sony" loading="lazy"  className="w-12 md:w-24" />
         </li>
         <li>
-          <img src="./asus.png" alt="Asus" className="w-12 md:w-24" />
+          <img src="./asus.png" alt="Asus" loading="lazy"  className="w-12 md:w-24" />
         </li>
         <li>
-          <img src="./redmi.png" alt="Redmi" className="w-12 md:w-24" />
+          <img src="./redmi.png" alt="Redmi" loading="lazy"  className="w-12 md:w-24" />
         </li>
         <li>
-          <img src="./bose.png" alt="Bose" className="w-12 md:w-24" />
+          <img src="./bose.png" alt="Bose" loading="lazy"  className="w-12 md:w-24" />
         </li>
         <li>
-          <img src="./hp.png" alt="Hp" className="w-12 md:w-24" />
+          <img src="./hp.png" alt="Hp" loading="lazy"  className="w-12 md:w-24" />
         </li>
       </ul>
     </div>
